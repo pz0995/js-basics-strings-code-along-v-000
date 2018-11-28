8b3d502892b927995e01659a9d9e62e0d02f0877
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-11-28 07:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = "Welcome to Flatbrook, " + currentUser +'!';
+const excitedWelcomeMessage = "WELCOME TO FLATBROOK, " + currentUser+"!";
+const shortGreeting = "Welcome, "+ currentUser +"!";
